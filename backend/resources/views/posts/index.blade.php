f40d83a426cb2a929a2d0510f2cf2d1724bbfe44
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-25 09:37:45 +0900

    投稿詳細、一覧の修正

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">投稿一覧</div>
-                    @foreach($posts as $post)
-                    <a href="{{ url('posts/' . $post->id) }}">
-                      <div class="card">
-                        <div class="card-body">
-                           <h2 class="card-title">{{ $post->title }}</h2>
-                           <p class="card-text">{{ $post->text }}</p>
-                           <p class="card-text">{{ $post->user->name }}</p>
-                           <p class="card-text">{{ $post->published }}</p>
-                        </div>
-                      </div>
-                    </a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+  <div class="row justify-content-center">
+    <div class="col-sm">
+        <!--投稿一覧-->
+        @foreach($posts as $post)
+          <a href="{{ url('posts/' . $post->id) }}" style="text-decoration: none;">
+          <div class="card d-inline-block" style="width: 18rem;">
+             <img src="..." class="card-img-top" alt="...">
+             <div class="card-body">
+                <h2 class="card-title" style="color:black;">{{ $post->title }}</h2>
+                 <p class="card-text"><small class="text-muted">{{ $post->text }}</small></p>
+                 <p class="card-text"><small class="text-muted">{{ $post->user->name }}</small></p>
+                 <p class="card-text"><small class="text-muted">{{ $post->created_at }}</small></p>
+             </div>
+          </div>
+          </a>
+        @endforeach
     </div>
+  </div>
 </div>
 @endsection
+
+
--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -1,35 +1,33 @@
 @extends("layouts.app")
 @section("content")
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                詳細画面
-            </div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card mb-3">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
             <div class="card-body">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <th>image</th>
-                            <td>{{$post->img}}</td>
-                        </tr>
-                        <tr>
-                            <th>title</th>
-                            <td>{{$post->title}}</td>
-                        </tr>
-                        <tr>
-                            <th>text</th>
-                            <td>{{$post->text}}</td>
-                        </tr>
-                        <tr>
-                            <th>date</th>
-                            <td>{{$post->published}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <a href="{{ url('/') }}" class="btn btn-info">戻る</a>
+                <h1 class="card-title">{{ $post->title }}</h1>
+                <p class="card-text"><small class="text-muted">{{ $post->user->name }}</small></p>
+                <p class="card-text"><small class="text-muted">{{ $post->created_at }}</small></p>
+                <p class="card-text" style="font-size: 20px;">{{ $post->text }}</p>
             </div>
+
+         @if(Auth::id() === $post->user_id)
+        <div class="row justify-content-center">
+            <a href="{{ url('posts/' . $post->id . '/edit') }}" class="btn btn-primary">編集</a>
+            <form method="POST" action="/posts/{{ $post->id }}">
+                @csrf 
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">削除</button>
+            </form>
+        </div>
+        @endif
+
+          </div>
         </div>
     </div>
 </div>
 @endsection
+
+
