98e2837048e427da4567881887b667570cd5017c
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-27 02:18:52 +0900

    コメント機能の実装

--- backend/app/Http/Controllers/CommentsController.php
+++ backend/app/Http/Controllers/CommentsController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+use App\Post;
+use App\Comment;
+
 class CommentsController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $comments = new Comment;
+        $comments->user_id     =    Auth::user()->id;
+        $comments->post_id     =    $request->post_id;
+        $comments->text        =    $request->text;
+        $comments->save();
+        return redirect('posts/' . $comments->post_id);
+    }
 }
--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -13,18 +13,42 @@
                 <p class="card-text" style="font-size: 20px;">{{ $post->text }}</p>
             </div>
 
-         @if(Auth::id() === $post->user_id)
-        <div class="row justify-content-center">
-            <a href="{{ url('posts/' . $post->id . '/edit') }}" class="btn btn-club-green">編集</a>
-            <form method="POST" action="/posts/{{ $post->id }}">
-                @csrf 
-                @method('DELETE')
-                <button class="btn btn-club-blue" type="submit">削除</button>
-            </form>
+            @if(Auth::id() === $post->user_id)
+            <div class="row justify-content-center">
+                <a href="{{ url('posts/' . $post->id . '/edit') }}" class="btn btn-club-green">編集</a>
+                <form method="POST" action="/posts/{{ $post->id }}">
+                    @csrf 
+                    @method('DELETE')
+                    <button class="btn btn-club-blue" type="submit">削除</button>
+                </form>
+            </div>
+            @endif
         </div>
-        @endif
 
-          </div>
+        <!-- コメント -->
+        <div class="p-3">
+
+            <!-- コメントフォーム -->
+            @if(Auth::check())
+            <form method="POST" action="/comments">
+                @csrf 
+                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                <input type="hidden" name="user_id" value="{{ $post->user->id }}">
+                <div class="form-group">
+                    <textarea class="form-control" name="text" placeholder="コメント"></textarea>
+                </div>
+                <button class="btn btn-club-green d-block" type="submit" style="margin: 0 auto;">投稿</button>
+            </form>
+            @endif
+            <!-- コメント表示 -->
+            <ul>
+                @forelse ($post->comments as $comment)
+                <p>{{ $comment->user->name }}</p>
+                <li>{{ $comment->text }}</li>
+                @empty
+                <li>コメントはまだありません</li>
+                @endforelse
+            </ul>
         </div>
     </div>
 </div>
