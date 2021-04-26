ab33e80372c7247777afc389885aee9154103c68
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-27 01:55:44 +0900

    コメント機能実装の準備

--- backend/app/Comment.php
+++ backend/app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo(\App\User::class, 'user_id');
+    }
+
+    public function post() {
+        return $this->belongsTo(\App\Post::class, 'post_id');
+    }
 }
--- backend/app/Post.php
+++ backend/app/Post.php
@@ -11,4 +11,8 @@ class Post extends Model
     public function user() {
         return $this->belongsTo(\App\User::class, 'user_id');
     }
+
+    public function comments() {
+        return $this->hasMany(\App\Comment::class,'post_id', 'id');
+    }
 }
