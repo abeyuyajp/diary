016f4920b534bbf0b348655cbaff7da05347f902
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-28 13:45:30 +0900

    ビューの修正

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -6,13 +6,12 @@
         <!--投稿一覧-->
         @foreach($posts as $post)
           <a href="{{ url('posts/' . $post->id) }}" style="text-decoration: none;">
-          <div class="card d-inline-block rounded" style="width: 18rem;">
+          <div class="card d-inline-block m-2" style="width: 18rem;">
              <img src="storage/image/{{$post->image}}" class="card-img-top" width="100%">
              <div class="card-body">
                 <h2 class="card-title" style="color:black;">{{ $post->title }}</h2>
                  <p class="card-text"><small class="text-muted">{{ Str::limit( $post->text, 100 ) }}</small></p>
-                 <p class="card-text"><small class="text-muted">{{ $post->user->name }}</small></p>
-                 <p class="card-text"><small class="text-muted">{{ $post->created_at }}</small></p>
+                 <p class="card-text"><small class="text-muted">{{ $post->created_at->format('Y/m/d') }}</small></p>
              </div>
           </div>
           </a>
--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -4,60 +4,63 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-          <div class="card mb-3">
-            <img src="{{ asset('storage/image/' . $post->image) }}" class="card-img-top" width="100%" height="300">
-            <div class="card-body">
-                <h1 class="card-title">{{ $post->title }}</h1>
-                <p class="card-text"><small class="text-muted">{{ $post->user->name }}</small></p>
-                <p class="card-text"><small class="text-muted">{{ $post->created_at }}</small></p>
-                <p class="card-text" style="font-size: 20px;">{{ $post->text }}</p>
-            </div>
-
-            @if(Auth::id() === $post->user_id)
-            <div class="row justify-content-center">
-                <a href="{{ url('posts/' . $post->id . '/edit') }}" class="btn btn-club-green">編集</a>
-                <form method="POST" action="/posts/{{ $post->id }}">
-                    @csrf 
-                    @method('DELETE')
-                    <button class="btn btn-club-blue" type="submit">削除</button>
-                </form>
-            </div>
-            @endif
-        </div>
-
-        <div class="p-3">
-            <!-- コメントフォーム -->
-            @if(Auth::check())
-            <form method="POST" action="/comments">
-                @csrf 
-                <input type="hidden" name="post_id" value="{{ $post->id }}">
-                <input type="hidden" name="user_id" value="{{ $post->user->id }}">
-                <div class="form-group d-flex">
-                    <textarea class="form-control" name="text" placeholder="日記にメモを残しましょう"></textarea>
-                    <input class="btn btn-club-green" type="submit" value="GO">
+            <div class="card mb-5">
+                <img src="{{ asset('storage/image/' . $post->image) }}" class="card-img-top" width="100%" height="40%">
+                <div class="card-body">
+                    <h1 class="card-title">{{ $post->title }}</h1>
+                    <div class="d-flex justify-content-between">
+                          <p class="card-text"><small class="text-muted">{{ $post->created_at->format('Y/m/d') }}</small></p>
+                          <ul class="navbar-nav">
+                              <li class="nav-item dropdown" style="list-style: none;">
+                                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  more
+                                  </a>
+                                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                      <a class="dropdown-item" href="{{ url('posts/' . $post->id . '/edit') }}">Edit</a>
+                                      <form method="POST" action="/posts/{{ $post->id }}">
+                                          @csrf 
+                                          @method('DELETE')
+                                          <button class="dropdown-item" type="submit">Delete</button>
+                                      </form>
+                                  </div>
+                              </li>
+                          </ul>
+                  </div>
+                    <p class="card-text" style="font-size: 20px;">{{ $post->text }}</p>
                 </div>
-            </form>
-            @endif
+            </div>
+            <div class="p-3">
+                <!-- コメントフォーム -->
+                @if(Auth::check())
+                    <form method="POST" action="/comments" style="margin-bottom: 15vh;">
+                        @csrf 
+                        <input type="hidden" name="post_id" value="{{ $post->id }}">
+                        <input type="hidden" name="user_id" value="{{ $post->user->id }}">
+                        <div class="form-group d-flex" >
+                            <textarea class="form-control" name="text" placeholder="日記にメモを残しましょう"></textarea>
+                            <input class="btn btn-club-green" type="submit" value="GO">
+                        </div>
+                    </form>
+                @endif
 
-            <!-- コメント表示 -->
-            <div class="mx-auto" style="width: 80%;">
-                <h2>MEMO</h2>
-                @forelse ($post->comments as $comment)
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th scope="row" style="padding: 3vh 3vw;">{{ $comment->user->name }}</th>
-                                <td>{{ $comment->text }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                @empty
-                    <p>メモはまだありません</p>
-                @endforelse
+                <!-- コメント表示 -->
+                <div class="mx-auto" style="width: 80%;">
+                    <h2>MEMO</h2>
+                    @forelse ($post->comments as $comment)
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" style="padding: 3vh 3vw;"><small class="text-muted">{{ $comment->created_at->diffForHumans() }}</small></th>
+                                    <td>{{ $comment->text }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    @empty
+                        <p>メモはまだありません</p>
+                    @endforelse
+                </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
-
-
