555813f66012ec08140e42ed18a4f4e72970b139
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-28 18:00:15 +0900

    トップページを作成

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'PostsController@index');
+Route::get('/posts', 'PostsController@index');
 Route::get('posts/create','PostsController@create');
 Route::post('posts', 'PostsController@store');
 Route::get('posts/{id}', 'PostsController@show');
