3d6e3f17d351207e2ab42f73673a9c880adb0e7b
abeyuyajp <yellowmonkey0618@gmail.com>
2021-05-03 00:08:31 +0900

    トップページを編集

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -37,39 +37,36 @@
                 <a class="btn btn-club-blue btn-lg  m-2" href="{{ route('register') }}" role="button">新規登録</a>
             </div>
         </div>
-        <div class="container">
+        <div class="container marketing">
             <div class="text-center">
-                <h4>英語日記アプリ</h4>
                 <h5>E-mail：test@example</br>Password：test0618</h5>
             </div>
 
-            <div class="row">
-                <div class="col-sm">
-                    <div class="card m-2" style="width: 18rem;">
-                        <img src="storage/image/blog-1.png" class="card-img-top" width="100%">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
+            <div class="row mt-5">
+                <div class="col-lg-4 text-center">
+                    <img src="storage/image/card-1.png" class="rounded-circle" width="140" height="140">
+                    <h2>今日の出来事を英語で投稿しましょう</h2>
+                    <p>
+                        楽しかったこと、嬉しかったこと、悲しかったこと、勉強したことなど貴重な体験を
+                        英語で残しましょう。英語で綴る習慣をつけることで、1日を振り返れるだけでなく、
+                        英語力も同時に身につけることができます。
+                    </p>
                 </div>
-                <div class="col-sm">
-                    <div class="card m-2" style="width: 18rem;">
-                        <img src="storage/image/blog-1.png" class="card-img-top" width="100%">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
+                <div class="col-lg-4 text-center">
+                    <img src="storage/image/card-2.png" class="rounded-circle" width="140" height="140">
+                    <h2>投稿は自分だけが見ることができます</h2>
+                    <p>
+                        あなたが投稿した内容は、他のユーザーには公開されません。検索もされません。
+                        完全プライペート空間で日記を楽しむことができます。
+                    </p>
                 </div>
-                <div class="col-sm">
-                    <div class="card m-2" style="width: 18rem;">
-                        <img src="storage/image/blog-1.png" class="card-img-top" width="100%">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
+                <div class="col-lg-4 text-center">
+                    <img src="storage/image/card-3.png" class="rounded-circle" width="140" height="140">
+                    <h2>分からなかった単語や文法はメモしておきましょう</h2>
+                    <p>
+                        Journalyにはメモ機能があります。この機能を活用することで、あなたの英語力の可能性は
+                        さらに広がります。分からなかった単語や文法はこのメモ機能を使って、記録に残しておきましょう。
+                    </p>
                 </div>
             </div>
         </div>
